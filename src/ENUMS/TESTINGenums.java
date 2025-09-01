9cc76bd48e425cd42267ae0d386b963ce1a144ee
raoyakub <yakubrao946@gmail.com>
2025-09-01 19:45:58 +0530

    adding more

---
+++ src/ENUMS/TESTINGenums.java
@@ -0,0 +1,10 @@
+package ENUMS;
+
+public class TESTINGenums {
+    public static void main(String[] args) {
+        TrafficLight color = TrafficLight.RED;
+        color = TrafficLight.GREEN;
+        Grade grade = Grade.C;
+    }
+
+}
