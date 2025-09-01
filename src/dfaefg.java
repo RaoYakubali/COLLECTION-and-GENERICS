f6909da02f7f6ded4668f27f2472862f8208a134
raoyakub <yakubrao946@gmail.com>
2025-09-01 19:53:18 +0530

    adding more

---
+++ src/dfaefg.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class dfaefg {
+
+    public static void main(String[] args) {
+        // List Example
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Yakub");
+        list.add("Ali");
+        list.add("Engineer");
+        list.remove("Ali");
+       
+
+        System.out.println("List: " + list);
+    }
+}
